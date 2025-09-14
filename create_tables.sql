0158eed0131e2e3c98c3cc4d57b163515ab655f5
Ariel <berinsteinariel@gmail.com>
2025-09-14 20:57:21 -0300

    nueva funcion para validar transaccion

--- create_tables.sql
+++ create_tables.sql
@@ -260,6 +260,46 @@ CREATE INDEX IDX_SNAPSHOT_ITEM_DATE ON ITEM_DAILY_SNAPSHOT (ITEM_ID, SNAPSHOT_DA
 -- ==============================================================
 
 -- ==============================================================
+-- Función para actualizar el stock del item al realizarse una transaccion
+-- ==============================================================
+CREATE OR REPLACE FUNCTION validate_transation_status_stock()
+    RETURNS TRIGGER AS $$
+DECLARE
+    current_stock ITEM.STOCK_QUANTITY%TYPE;
+    item_status ITEM.STATUS%TYPE;
+BEGIN
+    -- Obtener stock actual del ítem
+    SELECT STOCK_QUANTITY, STATUS
+    INTO current_stock, item_status
+    FROM ITEM
+    WHERE ITEM_ID = NEW.ITEM_ID
+    FOR UPDATE;
+    -- Lock para evitar race conditions,
+    -- es decir, que otro usuario también intente comprar el mismo item al mismo tiempo
+
+    -- Validar si el estado item está activo y se puede comprar
+    IF item_status != 'ACTIVE' THEN
+        RAISE EXCEPTION 'No se puede comprar un item que no está activo. Item_ID=%, estado actual=%',
+            NEW.ITEM_ID, item_status;
+    END IF;
+
+    -- Validar stock suficiente
+    IF NEW.QUANTITY > current_stock THEN
+        RAISE EXCEPTION 'No hay stock suficiente para el ítem %. Stock disponible: %, cantidad solicitada: %',
+            NEW.ITEM_ID, current_stock, NEW.QUANTITY;
+    END IF;
+
+    -- Actualizar en caso de que haya stock suficiente
+    UPDATE ITEM
+    SET STOCK_QUANTITY = current_stock - NEW.QUANTITY
+    WHERE ITEM_ID = NEW.ITEM_ID;
+
+    RETURN NEW;
+
+END;
+$$ LANGUAGE plpgsql;
+
+-- ==============================================================
 -- Función para actualizar UPDATED_AT automáticamente cuando se modifica cualquier campo de un registro
 -- ==============================================================
 CREATE OR REPLACE FUNCTION set_updated_at()
@@ -283,6 +323,14 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+-- ==========================================
+-- Trigger para ejecutar la validación antes de INSERT
+-- ==========================================
+CREATE TRIGGER trg_check_stock_before_order
+    BEFORE INSERT ON ORDER_TABLE
+    FOR EACH ROW
+EXECUTE FUNCTION validate_transation_status_stock();
 
 -- ==============================================================
 -- Triggers para UPDATED_AT
